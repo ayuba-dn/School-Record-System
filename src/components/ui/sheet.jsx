8bc8d4df4e5704c1dbf459dde2d57a2253c6cad4
stephen james <jeratdevworks@gmail.com>
2025-03-02 02:04:32 +0100

    close button adjusment

--- src/components/ui/sheet.jsx
+++ src/components/ui/sheet.jsx
@@ -14,20 +14,23 @@ const SheetClose = SheetPrimitive.Close;
 
 const SheetPortal = SheetPrimitive.Portal;
 
-const SheetOverlay = React.forwardRef(({ className, ...props }, ref) => (
-  <SheetPrimitive.Overlay
-    className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className,
-    )}
-    {...props}
-    ref={ref}
-  />
-));
+const SheetOverlay = React.forwardRef(
+  ({ className, overlayStyles, ...props }, ref) => (
+    <SheetPrimitive.Overlay
+      className={cn(
+        "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        className,
+        overlayStyles,
+      )}
+      {...props}
+      ref={ref}
+    />
+  ),
+);
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500",
+  "fixed z-50 gap-4 bg-background shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out",
   {
     variants: {
       side: {
@@ -38,52 +41,84 @@ const sheetVariants = cva(
         right:
           "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
       },
+      size: {
+        default: "p-6",
+        sm: "p-4",
+        lg: "p-8",
+        full: "p-0",
+      },
     },
     defaultVariants: {
       side: "right",
+      size: "default",
     },
   },
 );
 
 const SheetContent = React.forwardRef(
-  ({ side = "right", className, children, ...props }, ref) => (
+  (
+    {
+      side = "right",
+      size = "default",
+      className,
+      children,
+      overlayStyles,
+      closeButton = (
+        <SheetPrimitive.Close
+          className={cn(
+            "absolute opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:bg-secondary pointer-events-none",
+            side === "right" && "right-4 top-4",
+            side === "left" && "left-4 top-4",
+            side === "top" && "right-4 top-4",
+            side === "bottom" && "right-2 bottom-2",
+          )}
+        >
+          <X className="h-4 w-4 pointer-events-auto" />
+          <span className="sr-only">Close</span>
+        </SheetPrimitive.Close>
+      ),
+      ...props
+    },
+    ref,
+  ) => (
     <SheetPortal>
-      <SheetOverlay />
+      <SheetOverlay overlayStyles={overlayStyles} />
       <SheetPrimitive.Content
         ref={ref}
-        className={cn(sheetVariants({ side }), className)}
+        className={cn(sheetVariants({ side, size }), className)}
         {...props}
       >
         {children}
-        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </SheetPrimitive.Close>
+        {closeButton}
       </SheetPrimitive.Content>
     </SheetPortal>
   ),
 );
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
-const SheetHeader = ({ className, ...props }) => (
+const SheetHeader = ({ className, customHeader, ...props }) => (
   <div
     className={cn(
       "flex flex-col space-y-2 text-center sm:text-left",
       className,
     )}
     {...props}
-  />
+  >
+    {customHeader ? customHeader : props.children}
+  </div>
 );
 SheetHeader.displayName = "SheetHeader";
 
-const SheetFooter = ({ className, ...props }) => (
+const SheetFooter = ({ className, customFooter, ...props }) => (
   <div
     className={cn(
       "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
       className,
     )}
     {...props}
-  />
+  >
+    {customFooter ? customFooter : props.children}
+  </div>
 );
 SheetFooter.displayName = "SheetFooter";
 
